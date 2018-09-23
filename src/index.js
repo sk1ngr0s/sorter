86b5c9a8726c50842167783db707bdb678b0bb7b
Eugene <levchuk.eugene@gmail.com>
2018-09-23 15:08:25 +0300

    solution-Sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.sorterArr = [];
+    this.compareFunc = (left, right) => left - right;
   }
 
   add(element) {
     // your implementation
+    this.sorterArr.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.sorterArr[index];
   }
 
   get length() {
     // your implementation
+    return this.sorterArr.length;
   }
 
   toArray() {
     // your implementation
+    return this.sorterArr;
   }
 
   sort(indices) {
     // your implementation
+    let indArr = [];
+    for (let i = 0; i < indices.length; i++) {
+      indArr.push(this.sorterArr[indices[i]]);
+    }
+    indices.sort();
+    indArr.sort(this.compareFunc);
+    for (let j = 0; j < indices.length; j++) {
+      this.sorterArr[indices[j]] = indArr[j];
+    }
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunc = compareFunction;
   }
 }
 
